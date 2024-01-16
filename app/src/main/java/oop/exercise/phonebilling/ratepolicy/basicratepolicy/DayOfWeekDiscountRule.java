254cc737f56e85acc47d86d5eda966d070a0f8a3
fromitive <fromitive@gmail.com>
2024-01-16 14:39:04 +0900

    feat: DayOfWeekDiscountPolicy 추가

---
+++ app/src/main/java/oop/exercise/phonebilling/ratepolicy/basicratepolicy/DayOfWeekDiscountRule.java
@@ -0,0 +1,30 @@
+package oop.exercise.phonebilling.ratepolicy.basicratepolicy;
+
+import oop.exercise.common.Money;
+import oop.exercise.phonebilling.DateTimeInterval;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+
+public class DayOfWeekDiscountRule {
+    private List<DayOfWeek> dayOfWeeks = new ArrayList<>();
+    private Duration duration = Duration.ZERO;
+    private Money amount = Money.ZERO;
+
+    public DayOfWeekDiscountRule(List<DayOfWeek> dayOfWeeks, Duration duration, Money amount) {
+        this.dayOfWeeks = dayOfWeeks;
+        this.duration = duration;
+        this.amount = amount;
+    }
+
+    public Money calculate(DateTimeInterval interval) {
+        if (dayOfWeeks.contains(interval.getFrom().getDayOfWeek())) {
+            return amount.times(interval.duration().getSeconds() / duration.getSeconds());
+        }
+        return Money.ZERO;
+    }
+
+}
