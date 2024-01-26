ec04c805f3ea544ba70a624cef8dfd2624e639fd
fromitive <fromitive@gmail.com>
2024-01-26 17:13:35 +0900

    refactor: 접근제어 일관성 유지

--- app/src/main/java/oop/exercise/pizzastore/Pizza.java
+++ app/src/main/java/oop/exercise/pizzastore/Pizza.java
@@ -9,15 +9,15 @@ public abstract class Pizza {
         this.regionName = regionName;
     }
 
-    protected final void bake() {
+    public final void bake() {
         System.err.println("175도에서 25분간 굽기");
     }
 
-    protected final void cut() {
+    public final void cut() {
         System.out.println("피자를 사선으로 자르기");
     }
 
-    protected final void box() {
+    public final void box() {
         System.err.println("상자에 피자 담기");
     }
 
